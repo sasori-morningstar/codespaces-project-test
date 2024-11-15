9d4cebbeb15df0b5fd7845e1f6e3cd6fc4cbf78c
Ilyes Medjedoub <116976342+sasori-morningstar@users.noreply.github.com>
2024-11-15 02:11:17 +0000

    Changes the Home, About and Footer images and checking style

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/developer-junior.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Passionate junior developer holding laptop";
 
 /**
  * Project list
