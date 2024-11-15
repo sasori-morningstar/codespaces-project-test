d4d8646c07c4096362bb1f15bd8df84cfaa85b84
Ilyes Medjedoub <116976342+sasori-morningstar@users.noreply.github.com>
2024-11-15 01:39:12 +0000

    Changed informations value

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Ilyes Medjedoub",
+  title: "Full stack developer",
+  email: "ilyes.medjedoub07@gmail.com",
+  gitHub: "sasori-morningstar",
+  instagram: "sasori.morningstar",
+  linkedIn: "ilyes.medjedoub",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "pixeldudescollection",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
